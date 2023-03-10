2f3e103f71e1db79f3c23e78d4559c88a10dc1f7
Lucas Christian <lucas.christian.programmer@gmail.com>
2023-03-10 15:03:59 -0300

    Updated

--- components/dialog/index.tsx
+++ components/dialog/index.tsx
@@ -27,10 +27,10 @@ export function Dialog({ srcSet, sizes, src, alt, height, width }: DialogProps) 
         <button>
           <svg 
             xmlns="http://www.w3.org/2000/svg" fill="none" 
-            viewBox="0 0 24 24" stroke-width="1.5" 
+            viewBox="0 0 24 24" strokeWidth="1.5" 
             stroke="currentColor"
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>            
         </button>
       </form>
