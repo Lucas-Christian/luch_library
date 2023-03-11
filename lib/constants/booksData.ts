1447b876a6f9a352f90b81cbf94117a869832342
Lucas Christian <lucas.christian.programmer@gmail.com>
2023-03-11 14:03:09 -0300

    Updated

--- lib/constants/booksData.ts
+++ lib/constants/booksData.ts
@@ -1,10 +1,12 @@
 export type BookData = {
   categories: string;
   ariaLabel: string;
+  download: string;
   authors: string[];
   height: number;
   width: number;
   title: string;
+  read: string;
   src: string;
   alt: string;
   id: string;
@@ -14,10 +16,12 @@ export const booksData = {
   software_engineering_a_professional_approach: {
     categories: "software_development software_engineering",
     ariaLabel: "Livro Engenharia de Software uma abordagem profissional",
+    download: "https://drive.google.com/uc?id=1Dew5uoCNkSYpxdDtqKkVdag_XAtm6x9a&export=download",
     authors: ["Roger S. Pressman", "Bruce R. Maxim"],
     height: 406,
     width: 314,
     title: "Engenharia de Software\nUma abordagem profissional",
+    read: "https://drive.google.com/file/d/1Dew5uoCNkSYpxdDtqKkVdag_XAtm6x9a/view",
     src: "https://raw.githubusercontent.com/LordLuch/images/main/books/software_engineering_a_professional_approach.webp",
     alt: "Capa do livro Engenharia de Software uma abordagem profissional",
     id: "software_engineering_a_professional_approach",
@@ -25,10 +29,12 @@ export const booksData = {
   software_engineering_ian_sommerville: {
     categories: "software_development software_engineering",
     ariaLabel: "Livro Engenharia de Software por Ian Sommerville",
+    download: "https://drive.google.com/uc?id=1DfakjyKVX7LC9N2ZybPeBqRZ4veTQdL5&export=download",
     authors: ["Ian Sommerville"],
     height: 444,
     width: 330,
     title: "Engenharia de Software\nIan Sommerville",
+    read: "https://drive.google.com/file/d/1DfakjyKVX7LC9N2ZybPeBqRZ4veTQdL5/view",
     src: "https://raw.githubusercontent.com/LordLuch/images/main/books/software_engineering_ian_sommerville.webp",
     alt: "Capa do livro Engenharia de Software por Ian Sommerville",
     id: "software_engineering_ian_sommerville"
@@ -36,10 +42,12 @@ export const booksData = {
   human_computer_interaction: {
     categories: "software_development digital_design",
     ariaLabel: "Livro Interação Humano Computador",
+    download: "https://drive.google.com/uc?id=1pcgx8KzVR1xactop2UUqs6baC0r-ZxnI&export=download",
     authors: ["David Benyon"],
     height: 595,
     width: 397,
     title: "Interação Humano Computador",
+    read: "https://drive.google.com/file/d/1pcgx8KzVR1xactop2UUqs6baC0r-ZxnI/view",
     src: "https://raw.githubusercontent.com/LordLuch/images/main/books/human_computer_interaction.webp",
     alt: "Capa do livro Interação Humano Computador",
     id: "human_computer_interaction"
@@ -47,10 +55,12 @@ export const booksData = {
   the_smart_investor: {
     categories: "business",
     ariaLabel: "Livro O Investidor Inteligente",
+    download: "https://drive.google.com/uc?id=1vsWKbF2jSRZgpQTm9z92TvADDwye3-dB&export=download",
     authors: ["Benjamin Graham"],
     height: 486,
     width: 395,
     title: "O Investidor Inteligente",
+    read: "https://drive.google.com/file/d/1vsWKbF2jSRZgpQTm9z92TvADDwye3-dB/view",
     src: "https://raw.githubusercontent.com/LordLuch/images/main/books/the_smart_investor.webp",
     alt: "Capa do livro O Investidor Inteligente",
     id: "the_smart_investor"
--- pages/books/[id].tsx
+++ pages/books/[id].tsx
@@ -88,7 +88,7 @@ export default function Book({ id }: { id: keyof typeof booksData; }) {
           </table>
         </div>
         <div className={styles.actions}>
-          <a className="button_redirection" target="_blank" rel="nofollow" href="https://drive.google.com/file/d/1pcgx8KzVR1xactop2UUqs6baC0r-ZxnI/view?usp">
+          <a className="button_redirection" target="_blank" rel="nofollow" href={bookData.read}>
             <button>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
@@ -97,7 +97,7 @@ export default function Book({ id }: { id: keyof typeof booksData; }) {
               <p>Ler</p>
             </button>
           </a>
-          <a className="button_redirection" target="_blank" rel="nofollow" href="https://drive.google.com/uc?id=1pcgx8KzVR1xactop2UUqs6baC0r-ZxnI&export=download" download>
+          <a className="button_redirection" target="_blank" rel="nofollow" href={bookData.download} download>
             <button>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
